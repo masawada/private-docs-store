98133c6262e95d9ef56fb593e217317fa9cede91
Masayoshi Wada <developer@andantesoftware.com>
2021-08-19 02:06:12 +0900

    create a cloudfront web distribution and set the bucket as the origin

--- lib/private-docs-store-stack.ts
+++ lib/private-docs-store-stack.ts
@@ -1,10 +1,43 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as iam from '@aws-cdk/aws-iam';
 
 export class PrivateDocsStoreStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const bucket = new s3.Bucket(this, 'privateDocsBucket');
+
+    const storeIdentity = new cloudfront.OriginAccessIdentity(this, 'storeIdentity');
+    const bucketPolicyStatement = new iam.PolicyStatement({
+      actions: ["s3:GetObject"],
+      effect: iam.Effect.ALLOW,
+      principals: [
+        new iam.CanonicalUserPrincipal(
+          storeIdentity.cloudFrontOriginAccessIdentityS3CanonicalUserId
+        )
+      ],
+      resources: [`${bucket.bucketArn}/*`],
+    });
+    bucket.addToResourcePolicy(bucketPolicyStatement);
+
+    const cloudfrontDistribution = new cloudfront.CloudFrontWebDistribution(this, 'privateDocsDistribution', {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket,
+            originAccessIdentity: storeIdentity,
+          },
+          behaviors: [
+            {
+              isDefaultBehavior: true,
+            }
+          ],
+        },
+      ],
+    });
+
+    new cdk.CfnOutput(this, 'cloudfront', { value: cloudfrontDistribution.domainName });
   }
 }
