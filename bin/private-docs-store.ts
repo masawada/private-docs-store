7fae4b653b03de60f4bed72d45d91816ce45c575
Masayoshi Wada <developer@andantesoftware.com>
2021-08-19 01:14:06 +0900

    create the stack in the us-east-1 region

--- bin/private-docs-store.ts
+++ bin/private-docs-store.ts
@@ -5,17 +5,7 @@ import { PrivateDocsStoreStack } from '../lib/private-docs-store-stack';
 
 const app = new cdk.App();
 new PrivateDocsStoreStack(app, 'PrivateDocsStoreStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  // To use normal `lambda.Function` insteadof `EdgeFunction`, create the stack in the `us-east-1` region
+  // https://docs.aws.amazon.com/cdk/api/latest/docs/aws-cloudfront-readme.html#lambdaedge
+  env: { account: process.env.CDK_DEFAULT_ACOUNT, region: 'us-east-1' },
 });
